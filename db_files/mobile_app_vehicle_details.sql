fc516984b4edcc7f57799860403bce0908f3ee73
Justbishwas <b15hw45@gmail.com>
2023-12-16 15:02:07 +0545

    db files added

---
+++ db_files/mobile_app_vehicle_details.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mobile_app
+-- ------------------------------------------------------
+-- Server version	8.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `vehicle_details`
+--
+
+DROP TABLE IF EXISTS `vehicle_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vehicle_details` (
+  `vehicleDetails_id` int NOT NULL AUTO_INCREMENT,
+  `customer_id` int DEFAULT NULL,
+  `vehicle_type` varchar(45) DEFAULT NULL,
+  `vehicle_number` varchar(45) DEFAULT NULL,
+  `vehicle_lot_number` int DEFAULT NULL,
+  `vehicle_company` varchar(45) DEFAULT NULL,
+  `vehicle_model` varchar(45) DEFAULT NULL,
+  `bill_book_details` text,
+  `status` int DEFAULT '1',
+  PRIMARY KEY (`vehicleDetails_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_details`
+--
+
+LOCK TABLES `vehicle_details` WRITE;
+/*!40000 ALTER TABLE `vehicle_details` DISABLE KEYS */;
+INSERT INTO `vehicle_details` VALUES (1,1,'Two Wheeler','1234',12,'asdasd','asdasdasd','{\"createdDate\":\"asdasdasd\",\"expiryDate\":\"asdasdasd\",\"ownerName\":\"asdasda\",\"contactNumber\":\"asdasdasd\"}',0);
+/*!40000 ALTER TABLE `vehicle_details` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-16 14:55:22
