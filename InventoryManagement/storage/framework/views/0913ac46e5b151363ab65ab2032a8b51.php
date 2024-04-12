3c451a95dcba2917a15e9db6435e64be94f7bc29
Jayanti Bhandari <112817289+jayantibhandari@users.noreply.github.com>
2024-04-12 09:32:47 +0545

    remved ims

--- InventoryManagement/storage/framework/views/0913ac46e5b151363ab65ab2032a8b51.php
+++
@@ -1,7 +0,0 @@
-<?php echo $__env->make('repairpartseller.partials.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-<?php echo $__env->yieldContent('content'); ?>
-<?php echo $__env->yieldPushContent('js'); ?>
-<?php echo $__env->make('repairpartseller.partials.footer', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-
-<?php /**PATH D:\VG\VechicleGuardian\InventoryManagement\resources\views/repairpartseller/layouts/layout.blade.php ENDPATH**/ ?>
