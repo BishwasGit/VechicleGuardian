45d2a8fd039d5c84325f1c801e373b52f5a20a31
Jayanti Bhandari <112817289+jayantibhandari@users.noreply.github.com>
2024-04-23 20:22:41 +0545

    drawer and menu added on customer dashboard

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,29 +1,51 @@
 import "./App.css";
 import Welcome from "./components/Welcome";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/auth/login";
 import RegisterPage from "./components/auth/register";
-import { routes } from './components/includes/routes';
-import CustomerDashboard from './components/customer/dashboard';
-import RepaircenterDashboard from './components/repaircenters/dashboard';
-import WorkerDashboard from './components/workers/dashboard';
-import AdminDashboard from './components/admin/dashboard';
+import { routes } from "./components/includes/routes";
+import CustomerDashboard from "./components/customer/dashboard";
+import RepaircenterDashboard from "./components/repaircenters/dashboard";
+import WorkerDashboard from "./components/workers/dashboard";
+import AdminDashboard from "./components/admin/dashboard";
 
 function App() {
 	return (
 		<Router>
-		<div className="App">
-		  <Routes>
-			<Route exact path="/" element={<Welcome/>} />
-			<Route path="/login" element={<LoginPage/>} />
-			<Route path="/register" element={<RegisterPage/>} />
-			<Route path={routes.customerDashboard} element={<CustomerDashboard />} />
-			<Route path={routes.repairCenterDashboard} element={<RepaircenterDashboard />} />
-			<Route path={routes.workerDashboard} element={<WorkerDashboard />} />
-			<Route path={routes.adminDashboard} element={<AdminDashboard />} />
-		  </Routes>
-		</div>
-	  </Router>
+			<div className="App">
+				<Routes>
+					<Route
+						exact
+						path="/"
+						element={<Welcome />}
+					/>
+					<Route
+						path="/login"
+						element={<LoginPage />}
+					/>
+					<Route
+						path="/register"
+						element={<RegisterPage />}
+					/>
+					<Route
+						path={routes.customerDashboard}
+						element={<CustomerDashboard />}
+					/>
+					<Route
+						path={routes.repairCenterDashboard}
+						element={<RepaircenterDashboard />}
+					/>
+					<Route
+						path={routes.workerDashboard}
+						element={<WorkerDashboard />}
+					/>
+					<Route
+						path={routes.adminDashboard}
+						element={<AdminDashboard />}
+					/>
+				</Routes>
+			</div>
+		</Router>
 	);
 }
 
